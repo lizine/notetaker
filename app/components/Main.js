104a88165daf2e5a3bbc3c925ee25c974657fd9f
Nina Liljeblad <nina.liljeblad@gmail.com>
2015-12-26 16:32:35 +0200

    taking rendering ot of main component to app.js

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,5 +1,4 @@
 var React = require('react');
-var ReactDOM = require('react-dom');
 
 var Main = React.createClass ({
 	render: function (){
@@ -11,4 +10,4 @@ var Main = React.createClass ({
 	}
 });
 
-ReactDOM.render(<Main />, document.getElementById('app'));
+module.exports = Main;
