13286e947883472fb8366b204237fd5c928b5f9d
Nina Liljeblad <nina.liljeblad@gmail.com>
2015-12-26 17:17:32 +0200

    we now have home component rendering instructions and empty navigation

--- app/components/Home.js
+++ app/components/Home.js
@@ -9,4 +9,6 @@ var Home = React.createClass({
         )
 
     }
-});
+})
+
+module.exports = Home;
--- app/config/routes.js
+++ app/config/routes.js
@@ -3,9 +3,10 @@ var Main = require('../components/Main');
 var Home = require('../components/Home');
 var Router = require('react-router');
 var Route = Router.Route;
+var IndexRoute = Router.IndexRoute;
 
 module.exports = (
     <Route path="/" component={Main}>
-
+     <IndexRoute component={Home} />
         </Route>
 );
