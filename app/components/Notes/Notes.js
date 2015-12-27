c830dd74d37a7b7da076ab7decaa47fb33ca5014
Nina Liljeblad <nina.liljeblad@gmail.com>
2015-12-27 19:11:27 +0200

    adding noteslist component

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -1,11 +1,12 @@
 var React = require('react');
-
+var NotesList = require('./NotesList');
 var Notes = React.createClass({
     render:function(){
+  console.log('Notes:', this.props.notes);
         return (
             <div>
-                <p>Notesit</p>
-                <p>{this.props.notes}</p>
+               <h3>Notes for {this.props.username}</h3>
+                <NotesList notes={this.props.notes}/>
             </div>
         )
     }
