5521ef6347a5749f4b25e2e6d104187de9accee2
Nina Liljeblad <nina.liljeblad@gmail.com>
2015-12-29 21:05:09 +0200

    adding notes works now

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -24,6 +24,10 @@ var Profile = React.createClass({
     componentWillUnmount: function(){
         this.unbind('notes');
     },
+    handleAddNote: function(newNote){
+        //update firebase with newNote
+        this.ref.child(this.props.params.username).child(this.state.notes.length).set(newNote)
+    },
      render:function(){
          return (
              <div className="row">
@@ -34,7 +38,10 @@ var Profile = React.createClass({
                     <Repos username={this.props.params.username} repos={this.state.repos} />
                      </div>
                  <div className="col-md-4">
-                    <Notes username={this.props.params.username} notes={this.state.notes}/>
+                    <Notes
+                        username={this.props.params.username}
+                        notes={this.state.notes}
+                        addNote={this.handleAddNote}/>
                      </div>
                  </div>
 
