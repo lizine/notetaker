07ecabd8fcb1971e30314d8374cc354be656943f
Nina Liljeblad <nina.liljeblad@gmail.com>
2016-01-11 22:42:28 +0200

    searching from github caused page refresh, fixed

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -18,19 +18,26 @@ var Profile = React.createClass({
     },
     componentDidMount: function(){
         this.ref = new Firebase('https://brilliant-inferno-3892.firebaseio.com/');
-        var childRef = this.ref.child(this.props.params.username);
-        this.bindAsArray(childRef, 'notes');
-
-        helpers.getGithubInfo(this.props.params.username)
-        .then(function(data){
-            this.setState({
-                bio: data.bio,
-                repos: data.repos
-            })
-        }.bind(this))
+        this.init(this.props.params.username);
+    },
+    componentWillReceiveProps: function(nextProps){
+        this.unbind('notes');
+        this.init(nextProps.params.username);
     },
     componentWillUnmount: function(){
         this.unbind('notes');
+    },
+    init:function(username){
+        var childRef = this.ref.child(username);
+        this.bindAsArray(childRef, 'notes');
+
+        helpers.getGithubInfo(username)
+            .then(function(data){
+                this.setState({
+                    bio: data.bio,
+                    repos: data.repos
+                })
+            }.bind(this))
     },
     handleAddNote: function(newNote){
         //update firebase with newNote
